6d121e285ee2284f1307bb16711566c4262eb338
miyahara <kenmiya.sns@gmail.com>
2023-10-23 10:03:37 +0900

    [feat]楽天検索結果表示機能を追加

--- src/app/rakuten/urls.py
+++ src/app/rakuten/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='top'),
+    path('result', views.result, name='top'),
 ]
--- src/app/rakuten/views.py
+++ src/app/rakuten/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-  return render(request, 'top.html')
+  return render(request, 'top.html', {'keyword': ""})
+
+def result(request):
+  if request.method == 'POST':
+    keyword = request.POST.get('keyword', '')
+    api_url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
+    app_id = ''
+        
+    params = {
+      'format': 'json',
+      'keyword': keyword,
+      'applicationId': app_id,
+    }
+
+    response = requests.get(api_url, params=params)
+    data = response.json()
+
+    items = data['Items']
+  else:
+    items = []
+    keyword = ""
+  return render(request, 'top.html', {'items': items, 'keyword': keyword})
